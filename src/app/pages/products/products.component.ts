e1d478d4f431c8912c5ff5820cac421893d01b8e
MuchemiKelvin <kelvin.chuthi@zandaux.com>
2024-05-07 17:19:14 +0300

    products json

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+// home.component.ts
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[]=[]; // Define products array with Product interface
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    // Fetch product data from JSON file
+    this.http.get<Product[]>('assets/products.json').subscribe((data: Product[]) => {
+      this.products = data;
+      console.log({data})
+    });
+  }
+}
+
+// product.model.ts
+
+export interface Product {
+  name: string;
+  price: number;
+  imageUrl: string;
 }
